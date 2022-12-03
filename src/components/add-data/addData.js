6716453b6ec7f86fa90264639f83aa75fbb54ecd
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-12-03 11:56:58 +0100

    update

--- src/components/add-data/addData.js
+++ src/components/add-data/addData.js
@@ -12,12 +12,12 @@ export default function AddData() {
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="text" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="text" placeholder="Password" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                             <Form.Label>Example textarea</Form.Label>
