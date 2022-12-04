530656f1d045f5cde7006916273f0d1a03db4fe1
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-12-04 01:06:38 +0100

    update

--- src/components/data-list/dataList.js
+++ src/components/data-list/dataList.js
@@ -1,12 +1,34 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
-import dataListStyle from "./dataList.module.scss"
+import { Button } from "react-bootstrap";
+import dataListStyle from "./dataList.module.scss";
 
 export default function DataList() {
     return (
         <section>
             <div className={dataListStyle.holdAll}>
-                <h1>Data List</h1>
+                <div className={dataListStyle.holdContent}>
+                    <div>
+                        <h4>The Header Text</h4>
+                        <p>ewfefwefwefwefewfwefwewefwefwefwe</p>
+                        <p>Date</p>
+                    </div>
+                    <div className={dataListStyle.holdButton}>
+                        <Button className="mb-3" variant="success">View</Button>
+                        <Button variant="danger">Delete</Button>
+                    </div>
+                </div>
+                <div className={dataListStyle.holdContent}>
+                    <div>
+                        <h4>The Header Text</h4>
+                        <p>ewfefwefwefwefewfwefwewefwefwefwe</p>
+                        <p>Date</p>
+                    </div>
+                    <div className={dataListStyle.holdButton}>
+                        <Button className="mb-3" variant="success">View</Button>
+                        <Button variant="danger">Delete</Button>
+                    </div>
+                </div>
             </div>
         </section>
     );
